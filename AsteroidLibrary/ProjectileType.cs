d799fbe4a5d2297c666f9b20c81b328ae92cdaf9
Kevin <kevin.vd.vleuten@gmail.com>
2013-10-21 17:45:10 +0200

    adding ProjectileType class

---
+++ AsteroidLibrary/ProjectileType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsteroidLibrary
+{
+    public enum ProjectileType
+    {
+        Plasma = 0,
+
+        AmountOfProjectiles = 3
+    }
+}
