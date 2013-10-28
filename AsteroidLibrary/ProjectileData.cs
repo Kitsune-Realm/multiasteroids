ce9ad102ce75af5e6c810246ed7503085aa1cda5
Kevin <kevin.vd.vleuten@gmail.com>
2013-10-28 16:04:59 +0100

    adding file

---
+++ AsteroidLibrary/ProjectileData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace AsteroidLibrary
+{
+    public class ProjectileData
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float RotationAngle { get; set; }
+        public TcpClient Owner { get; set; }
+
+        public ProjectileData(float x, float y, float rotationangle)
+        {
+            this.X = x;
+            this.Y = y;
+            this.RotationAngle = rotationangle;
+        }
+
+        public ProjectileData(float x, float y, float rotationangle, TcpClient owner)
+        {
+            this.X = x;
+            this.Y = y;
+            this.RotationAngle = rotationangle;
+            this.Owner = owner;
+        }
+    }
+}
